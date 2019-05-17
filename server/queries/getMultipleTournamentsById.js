7a539fba7c3c7f646d99f64d03bb50a0eec83a69
Michael Hackman <reedhackman@gmail.com>
2019-05-17 01:16:51 -0700

    expanded tournament api

--- server/queries/getMultipleTournamentsById.js
+++ server/queries/getMultipleTournamentsById.js
@@ -3,6 +3,7 @@ const pool = require('./pool.js')
 const getMultipleTournamentsById = (req, res) => {
   let idsArray = JSON.parse(req.params.idsArray)
   let id = req.params.id
+  let resRows = []
   let sqlString = 'SELECT * FROM tournaments WHERE '
   if(idsArray.length){
     sqlString += 'tournament_id = ' + idsArray[0]
@@ -14,9 +15,21 @@ const getMultipleTournamentsById = (req, res) => {
   pool.query(sqlString, (err, data) => {
     if(err) throw err
     data.rows.forEach((tournament) => {
-      //
+      let count = 1
+      tournament.players.forEach((p) => {
+        if(p == id){
+          resRows.push({
+            id: tournament.tournament_id,
+            name: tournament.tournament_name,
+            date: (tournament.tournament_date.getMonth() + 1) + '/' + tournament.tournament_date.getDate() + '/' + tournament.tournament_date.getFullYear(),
+            totalPlayers: tournament.players.length,
+            playerPosition: count
+          })
+        }
+        count++
+      })
     })
-    res.status(200).json(data.rows)
+    res.status(200).json(resRows)
   })
 }
 
